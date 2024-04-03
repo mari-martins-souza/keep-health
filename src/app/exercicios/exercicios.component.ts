66aad31ac09acc04fc4201b83e748ca197179a44
Mariana Martins <marih_ms@hotmail.com>
2024-04-03 11:28:26 -0300

    atualizado exercicios component

--- src/app/exercicios/exercicios.component.ts
+++ src/app/exercicios/exercicios.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { DropdownModule } from 'primeng/dropdown';
@@ -7,44 +7,79 @@ import { DropdownModule } from 'primeng/dropdown';
 @Component({
   selector: 'app-exercicios',
   standalone: true,
-  imports: [ButtonModule, DialogModule, DropdownModule, FormsModule],
+  imports: [ButtonModule, DialogModule, DropdownModule, FormsModule, ReactiveFormsModule],
   templateUrl: './exercicios.component.html',
   styleUrl: './exercicios.component.scss'
 })
 
 export class ExerciciosComponent {
   visible: boolean = false;
-  modalidade!: string;
-  dataAtividade!: string;
-  distancia!: number;
-  duracao!: number;
 
-  @ViewChild('listaExercicios') listaExercicios!: ElementRef;
-  @ViewChild('dataAtividade') dataAtividadeInput!: ElementRef;
-  @ViewChild('distancia') distanciaInput!: ElementRef;
-  @ViewChild('duracao') duracaoInput!: ElementRef;
+  showDialog() {
+    this.visible = true;
+  
+  }
+
+  novaAtividadeForm = new FormGroup({
+    atividade: new FormControl('', Validators.required),
+    data:new FormControl('', Validators.required),
+    distancia:new FormControl(''),
+    duracao:new FormControl(''),
+  });
+
 
   incluirAtividade() {
-    this.modalidade = this.listaExercicios.nativeElement.value;
-    this.dataAtividade = this.dataAtividadeInput.nativeElement.value;
-    this.distancia = this.distanciaInput.nativeElement.valueAsNumber;
-    this.duracao = this.duracaoInput.nativeElement.valueAsNumber;
-
-    this.salvarAtividadeLocalStorage();
+    const formAtividade = {
+      atividade: this.novaAtividadeForm.value.atividade,
+      data: this.novaAtividadeForm.value.data,
+      distancia: this.novaAtividadeForm.value.distancia,
+      duracao: this.novaAtividadeForm.value.duracao,
+    }
+      this.salvarAtividadeLocalStorage();
   }
 
   salvarAtividadeLocalStorage() {
-    const atividade = {
-      modalidade: this.modalidade,
-      dataAtividade: this.dataAtividade,
-      distancia: this.distancia,
-      duracao: this.duracao,
-    };
-
-    localStorage.setItem('atividades', JSON.stringify(atividade));
+        const atividade = {
+          atividade: this.novaAtividadeForm.value.atividade,
+          data: this.novaAtividadeForm.value.data,
+          distancia: this.novaAtividadeForm.value.distancia,
+          duracao: this.novaAtividadeForm.value.duracao,
+        };
+        localStorage.setItem('atividades', JSON.stringify(atividade));
   }
 
-  showDialog() {
-      this.visible = true;
-  }
 }
+
+
+
+
+
+//   @ViewChild('listaExercicios') listaExercicios!: ElementRef;
+//   @ViewChild('dataAtividade') dataAtividadeInput!: ElementRef;
+//   @ViewChild('distancia') distanciaInput!: ElementRef;
+//   @ViewChild('duracao') duracaoInput!: ElementRef;
+
+//   incluirAtividade() {
+//     this.modalidade = this.listaExercicios.nativeElement.value;
+//     this.dataAtividade = this.dataAtividadeInput.nativeElement.value;
+//     this.distancia = this.distanciaInput.nativeElement.valueAsNumber;
+//     this.duracao = this.duracaoInput.nativeElement.valueAsNumber;
+
+//     this.salvarAtividadeLocalStorage();
+//   }
+
+//   salvarAtividadeLocalStorage() {
+//     const atividade = {
+//       modalidade: this.modalidade,
+//       dataAtividade: this.dataAtividade,
+//       distancia: this.distancia,
+//       duracao: this.duracao,
+//     };
+
+//     localStorage.setItem('atividades', JSON.stringify(atividade));
+//   }
+
+//   showDialog() {
+//       this.visible = true;
+//   }
+// }
